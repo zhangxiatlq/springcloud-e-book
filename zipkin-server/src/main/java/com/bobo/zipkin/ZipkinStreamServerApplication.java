2da3e0955dd09d0dfbcfc886d4d77096ce8acaf4
zhangxia_dell <654122192@qq.com>
2021-01-26 08:54:26 +0800

    添加zipkin服务（保存到内存中），展示spring-cloud-sleuth的数据

---
+++ zipkin-server/src/main/java/com/bobo/zipkin/ZipkinStreamServerApplication.java
@@ -0,0 +1,22 @@
+package com.bobo.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * @PROJECT:zipkin-server
+ * @Author:zhangxia
+ * @Desc:
+ * @Date: 13:26 2021/1/25
+ * @VERSION: 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinStreamServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinStreamServerApplication.class, args);
+    }
+}
